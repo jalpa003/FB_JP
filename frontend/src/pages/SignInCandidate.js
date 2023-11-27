ac6ea4fb3eb0f308e7b351bb9cbcd8e65aaccb1f
Jalpa Tank <70699020+jalpa003@users.noreply.github.com>
2023-11-26 21:46:55 -0500

    Update SignInCandidate.js

--- frontend/src/pages/SignInCandidate.js
+++ frontend/src/pages/SignInCandidate.js
@@ -38,7 +38,7 @@ function CandidateSignIn() {
     const handleSubmit = async (values) => {
         try {
             // Make API request to candidate sign-in endpoint
-            const response = await axios.post('http://localhost:3003/user_login', values);
+            const response = await axios.post('https://fb-jp-backend.onrender.com/user_login', values);
             if (response.status === 200) {
                 toast.success(response.data.message);
                 setSent(true);
